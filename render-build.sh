be0dcca1905885fd2b03c972707d5370a5f1ddf9
mary-therese <mariatheresayu17@gmail.com>
2025-10-16 13:34:36 +0800

    revert, added render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit  
 
 # Update and install system packages
-# apt-get update
-# apt-get install -y pandoc poppler-utils tesseract-ocr
+apt-get update
+apt-get install -y pandoc poppler-utils tesseract-ocr
 
 # Install Python packages
 pip3 install -r requirements.txt
