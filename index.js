5b44e35622ecc5fd0b45fc156e8d62be03fa2e29
mary-therese <mariatheresayu17@gmail.com>
2025-10-16 09:48:20 +0800

    added last validation on acronym, updated cors in index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // allow cookies/auth headers if needed
+  credentials: true, 
 }));
 
 app.use(express.json());
